81c591be2e195a3767db848ec92d8070324b25a6
Vikash Patel <72722514+vikashpatel714@users.noreply.github.com>
2022-09-23 11:42:22 +0530

    Add files via upload

---
+++ PRATICAL/DAY1/Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+    //class declaration named Hello
+
+    public static void main(String[] args) { //main method in Java
+//It is predefined method in Java
+
+        System.out.println("Hello World!");
+        //Display statement in Java
+//It is ended with semicolon
+
+    }//end of main method
+}//end of class
